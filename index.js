78684371ba8db87aa218b46500f18e7d412f31a0
fwdadri <areyes@fwdcostarica.com>
2024-08-14 09:33:24 -0600

    cambios

---
+++ index.js
@@ -0,0 +1,19 @@
+class auto{
+
+    constructor(marca, modelo, anoe){
+        this.marca=marca;
+        this.modelo=modelo;
+        this.anoe=anoe;
+    }
+
+    mostrarAuto(){
+        return `este vehiculo es ${this.marca} ${this.modelo}`//con dole comilla alrevez ctrl+alt+``
+    }
+
+} 
+
+const miAuto1= new auto('toyota','corolla',2024);
+const miAuto2= new auto('hiunday','elentra',2024);
+
+console.log(miAuto1.mostrarAuto())
+
